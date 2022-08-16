8371672659302f4c7ab87da2e8e54c2517c4981d
narvita <narkha21@gmail.com>
2022-08-16 11:32:56 +0400

    custom pipe

--- src/app/pipes/custom.pipe.ts
+++ src/app/pipes/custom.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomPipe implements PipeTransform {
 
-  transform(value: number, currencyType: string, currencyLocation: string = 'fr' || 'be', digitsNumber: number): string  {
-    if (currencyLocation == 'fr') {
+  transform(value: number, currencyType: string, location: string = 'fr' || 'be', digitsNumber: number): string  {
+    if (location == 'fr') {
       value = value / 1000;
       return currencyType + value.toFixed(digitsNumber);
     } else {
